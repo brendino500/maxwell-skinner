33bc447c924d46027468c609b02de3d92d2042d4
brendino500 <brenda.ty@live.com>
2020-12-09 17:47:34 +0000

    title and logo

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,7 +6,6 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   container: {
