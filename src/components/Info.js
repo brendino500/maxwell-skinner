42ee3d37be25e53ad1f3cb079a8e18c95d278aa1
brendino500 <brenda.ty@live.com>
2020-12-09 17:33:08 +0000

    mobile responsive

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,6 +6,7 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -18,26 +19,44 @@ const useStyles = makeStyles((theme) => ({
     letterSpacing: 5,
     color: "#403C34",
     margin: 15,
+    [theme.breakpoints.between("xs", "sm")]: {
+      marginTop: "40%",
+      zIndex: 1,
+    },
   },
   text: {
     fontFamily: "PT serif",
     fontSize: 18,
     margin: 15,
+    [theme.breakpoints.between("xs", "sm")]: {
+      // marginTop: "40%",
+      zIndex: 1,
+    },
   },
   inputText: {
     fontFamily: "PT serif",
     fontSize: 20,
     letterSpacing: 5,
+    [theme.breakpoints.between("xs", "sm")]: {
+      zIndex: 1,
+    },
   },
   input: {
     display: "flex",
     margin: 15,
+    [theme.breakpoints.between("xs", "sm")]: {
+      zIndex: 1,
+    },
   },
   button: {
     width: 200,
     fontFamily: "PT serif",
     fontSize: 18,
     letterSpacing: 2,
+    [theme.breakpoints.between("xs", "sm")]: {
+      width: 140,
+      zIndex: 1,
+    },
   },
   textField: {
     fontFamily: "PT serif",
@@ -90,7 +109,7 @@ export default function Info() {
               color="primary"
               className={classes.button}
             >
-              Yes please
+              Yes!
             </Button>
           </div>
         </form>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,19 @@ import Info from "./Info";
 import image from "../assets/andi-rieger-OhHVGV4EUmA-unsplash.jpg";
 
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import { Grid, Hidden } from "@material-ui/core/";
 
 const useStyles = makeStyles((theme) => ({
   image: {
     height: "97vh",
+  },
+  imageMobile: {
+    position: "Absolute",
+    zIndex: 0,
+    height: "80vh",
+    marginTop: "17%",
+    marginLeft: "1%",
+    opacity: 0.5,
   },
 }));
 
@@ -17,7 +25,12 @@ export default function Main() {
   return (
     <Grid container direction="row" justify="space-between" alignItems="center">
       <Info />
-      <img src={image} alt="model" className={classes.image} />
+      <Hidden smDown>
+        <img src={image} alt="model" className={classes.image} />
+      </Hidden>
+      <Hidden smUp>
+        <img src={image} alt="model" className={classes.imageMobile} />
+      </Hidden>
     </Grid>
   );
 }
